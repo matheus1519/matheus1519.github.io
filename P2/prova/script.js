5b3c09db3b2bfe425d638ed5d09d9897338f4e42
matheus1519 <1519matheus@gmail.com>
2019-08-30 23:58:46 -0300

    Prova Ajustes

--- P2/prova/script.js
+++ P2/prova/script.js
@@ -1,25 +1,56 @@
 $(document).ready(function(){
-	valor = $('select#quartos').change(addQuartos);
-	dias = $('#datachegada').on('change',calcularDias);
-	dias = $('#datasaida').on('change',calcularDias);
-	$('#atualizar').atualizar('click',calcularDias);
-
-	function calcularValor(dias) {
-		$('#adultos');
-	}
-
+	var dias = -1;
+	$('select#quartos').change(addQuartos);
+	$('#datachegada').on('change',calcularDias);
+	$('#datasaida').on('change',calcularDias);
+	$('#atualizar').on('click',calcularValor);
+	
 	function calcularDias() {
 		var a = new Date($('#datachegada').val());
 		var b = new Date($('#datasaida').val());
 		var c = b.getTime() - a.getTime();
-		var dias = Math.ceil(c / (1000 * 3600 * 24));
-		
-		return dias;
+		dias = Math.ceil(c / (1000 * 3600 * 24));
 	}
+	function calcularValor() {
+		tamanho = $('select#quartos').val();
+		var valor = 0;
+		
+		for (var i = 0; i < tamanho; i++) {
+			switch(Number($('#pessoas'+'-'+i).val()))
+			{
+				case 1:
+					valor += 139;
+					break;
+				case 2:
+					valor += 220;
+					break;
+				case 3:
+					valor += 340;
+					break;
+
+			}
+
+			if($('#criancas'+'-'+i).val()==1)
+			{
+				valor += 30;
+			}
+			
+		}
+
+		if(valor == 0 || dias == -1) 
+		{
+			$('#total').html('Valor total: Preencha tudo!'); 
+		}
+		else
+		{
+			$('#total').html('Valor total: R$'+valor*dias);
+		}
+		
+	}
+
 
 	function addQuartos() {
 		tamanho = $(this).val();
-		var primeira = true;
 
 		if($('#div-ad-cr').length != 0)
 		{
@@ -38,18 +69,18 @@ $(document).ready(function(){
 
 			$('#div-ad-cr').append(
 				'<h6 class="center col-sm-12">Quarto</h6>' +
-				'<div class="form-group col-sm-6" id="div-adultos'+'-'+i+1+'"></div>' +
-				'<div class="form-group col-sm-6" id="div-criancas'+'-'+i+1+'"></div>'
+				'<div class="form-group col-sm-6" id="div-pessoas'+'-'+i+'"></div>' +
+				'<div class="form-group col-sm-6" id="div-criancas'+'-'+i+'"></div>'
 			);
 
-			$('#div-adultos'+'-'+i+1).append(
-				'<label for="adultos" class="col-sm-6 col-form-label">Adultos:</label>'+
-				'<select class="form-control" id="adultos'+'-'+i+1+'"><option>Selecione Adultos</option><option>1</option><option>2</option><option>3</option></select>'
+			$('#div-pessoas'+'-'+i).append(
+				'<label for="pessoas" class="col-sm-6 col-form-label">Pessoas:</label>'+
+				'<select class="form-control" id="pessoas'+'-'+i+'"><option>Selecione as Pessoas</option><option>1</option><option>2</option><option>3</option></select>'
 			);
 
-			$('#div-criancas'+'-'+i+1).append(
+			$('#div-criancas'+'-'+i).append(
 				'<label for="criancas" class="col-sm-6 col-form-label">Criancas:</label>'+
-				'<select class="form-control" id="criancas'+'-'+i+1+'"><option>Selecione Crianças</option><option>1</option></select>'
+				'<select class="form-control" id="criancas'+'-'+i+'"><option>Selecione Crianças</option><option>1</option></select>'
 			);
 		}
 	}
