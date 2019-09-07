6000aed91f71aa9c1ae4aa3e091538f785f335a3
Matheus Almeida <1519academico@gmail.com>
2019-09-07 00:38:50 -0300

    ajustes final agora e serio

--- P2/Aula5/Exercicio 1/script.js
+++ P2/Aula5/Exercicio 1/script.js
@@ -4,12 +4,20 @@ function evento() {
 	conteudo = document.querySelector('#conteudo_'+num);
 	if(conteudo.style.display != 'none')
 	{
-		conteudo.style.display = 'none';
+		conteudo.style.transform = 'scaleY(0) translateY(-200px)';
+		setTimeout(function(){
+			conteudo.style.display = 'none';
+		},300);
 		enlace.text = 'Mostrar Conteúdo';
 	}
 	else
 	{
 		conteudo.style.display = 'block';
+		setTimeout(function(){
+			conteudo.style.transform = 'scaleY(1)';
+		},300);
+		
+		
 		enlace.text = 'Ocultar Conteúdo';
 	}
 
@@ -18,4 +26,13 @@ function evento() {
 
 for (var i = 1; i <= 3; i++) {
 	document.querySelector('#enlace_'+i).addEventListener('click',evento, false);
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
